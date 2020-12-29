e04512102c7d9f32d559dda676331aaa46067092
VBalaguera <61208719+VBalaguera@users.noreply.github.com>
2020-12-29 20:24:19 +0100

    more heroku testing

--- src/index.js
+++ src/index.js
@@ -2,11 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-
-// we need to bring here persist and the comp that will leverage inside our app that will give the context of our new persistReducer; 
 import { PersistGate } from 'redux-persist/integration/react';
-//this can be used for multiple platforms too 
-
 
 import { store, persistor } from './redux/store';
 
@@ -23,6 +19,3 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root')
 );
-
-//in persistor={persistor}, {persistor} is export const persistor = persistStore(store) from store.js 
-// we want to access the persistor flow, so we need to make sure to wrap the app; it will allow PersistGate to receive the Store, and also do actions that will rehydrate the State when our app refreshes; if we refresh after putting some items in the cart/reading, we will see the persist.REHYDRATE action in the console; which is great; 
