5c2d8bff85db0d288d42441910e5a5346a3d32d9
VBalaguera <61208719+VBalaguera@users.noreply.github.com>
2021-01-02 18:18:33 +0100

    removing redux logger from production build

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,11 +4,15 @@ import logger from 'redux-logger';
 
 import rootReducer from './root-reducer';
 
-const middlewares = [logger];
+const middlewares = [];
+
+//remember: inside Node there is the env variable. Create React App creates an env variable, only accessible through process.env; NODE_ENV is a prop that will be development, production, or test. 
 
 if (process.env.NODE_ENV === 'development') {
   middlewares.push(logger);
 }
+
+//when we call yarn build, it switches that node variable to production; while using yarn start, it switches it to development; 
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
