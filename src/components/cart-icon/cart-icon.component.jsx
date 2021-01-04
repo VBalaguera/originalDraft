bc266e5be19b1a153d2feec0b299df8df0a8eb9c
VBalaguera <61208719+VBalaguera@users.noreply.github.com>
2021-01-04 21:21:46 +0100

    styled-comps implementation phase 1

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -5,8 +5,6 @@ import { createStructuredSelector } from 'reselect';
 import { toggleCartHidden } from '../../redux/cart/cart.actions';
 import { selectCartItemsCount } from '../../redux/cart/cart.selectors';
 
-import { ReactComponent as ShoppingIcon } from '../../assets/11.1 shopping-bag.svg';
-
 import { CartIconStyled, ShoppingIconStyled, ItemCountStyled } from './cart-icon.styles'; 
 
 const CartIcon = ({ toggleCartHidden, itemCount }) => (
